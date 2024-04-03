ca2b8778a4d3a18bdad35205153945f0b766b9df
Sergey Shvetsov <s.shvetsov@pravo.tech>
2024-04-03 19:54:31 +0400

    v1.1 COALESCE

--- migrations/mart.f_customer_retention.sql
+++ migrations/mart.f_customer_retention.sql
@@ -1,112 +1,51 @@
 DELETE FROM mart.f_customer_retention
 WHERE period_id = DATE_PART('week', '{{ds}}'::TIMESTAMP);
 
-WITH temp1 AS (
+WITH customers AS (
     SELECT
-        fsl.*,
+        fsl. * ,
         dc.week_of_year,
         CASE WHEN fsl.payment_amount < 0 THEN 'refunded'
             ELSE 'shipped'
         END AS status
     FROM mart.f_sales fsl
     JOIN mart.d_calendar dc ON dc.date_id = fsl.date_id
-    ORDER BY fsl.customer_id
 ),
-new_customer_count AS (
-    SELECT
-        customer_id,
-        week_of_year,
+customer_data AS (
+    SELECT 
+        customer_id, 
+        week_of_year, 
         item_id,
-        COUNT(DISTINCT date_id) AS c11
-    FROM temp1
-    GROUP BY customer_id, week_of_year, item_id
-    HAVING COUNT(DISTINCT date_id) = 1
+        CASE 
+            WHEN status = 'shipped' AND COUNT(DISTINCT date_id) = 1 THEN 'new_customer'
+            WHEN status = 'shipped' AND COUNT(DISTINCT date_id) > 1 THEN 'returning_customer'
+            WHEN status = 'refunded' THEN 'refunded_customer'
+        END AS customer_type,
+        payment_amount
+    FROM customers
+    GROUP BY customer_id, week_of_year, item_id, status, payment_amount
 ),
-new_customer_count2 AS (
-    SELECT
-        week_of_year,
-        item_id,
-        COUNT(DISTINCT customer_id) AS new_customers_count
-    FROM new_customer_count
-    GROUP BY week_of_year, item_id
-),
-returning_customer_count AS (
-    SELECT
-        customer_id,
-        week_of_year,
-        item_id,
-        COUNT(DISTINCT date_id) AS returning_customers_count
-    FROM temp1
-    GROUP BY customer_id, week_of_year, item_id
-    HAVING COUNT(DISTINCT date_id) > 1
-),
-returning_customer_count2 AS (
-    SELECT
-        week_of_year,
-        item_id,
-        COUNT(DISTINCT customer_id) AS returning_customers_count
-    FROM returning_customer_count
-    GROUP BY week_of_year, item_id
-),
-refunded_customer AS (
-    SELECT
-        week_of_year,
-        item_id,
-        COUNT(DISTINCT customer_id) AS refunded_customer_count
-    FROM temp1
-    WHERE status = 'refunded'
-    GROUP BY week_of_year, item_id
-),
-new_customers_revenue AS (
-    SELECT
-        nc.week_of_year,
-        nc.item_id,
-        SUM(temp1.payment_amount) AS new_customers_revenue
-    FROM new_customer_count nc
-    LEFT JOIN temp1 ON nc.customer_id = temp1.customer_id AND nc.week_of_year = temp1.week_of_year AND nc.item_id = temp1.item_id
-    GROUP BY nc.week_of_year, nc.item_id
-),
-returning_customers_revenue AS (
-    SELECT
-        rtc.week_of_year,
-        rtc.item_id,
-        SUM(temp1.payment_amount) AS returning_customers_revenue
-    FROM returning_customer_count rtc
-    LEFT JOIN temp1 ON rtc.customer_id = temp1.customer_id AND rtc.week_of_year = temp1.week_of_year AND rtc.item_id = temp1.item_id
-    GROUP BY rtc.week_of_year, rtc.item_id
-),
-customer_refunded1 AS (
-    SELECT
-        customer_id,
-        week_of_year,
-        item_id,
-        COUNT(*) AS c
-    FROM temp1
-    WHERE temp1.status = 'refunded'
-    GROUP BY customer_id, week_of_year, item_id
-),
-customers_refunded2 AS (
-    SELECT
-        week_of_year,
-        item_id,
-        SUM(c) AS customers_refunded
-    FROM customer_refunded1
-    GROUP BY week_of_year, item_id
+revenue_data AS (
+    SELECT 
+        week_of_year, 
+        item_id, 
+        customer_type,
+        SUM(payment_amount) AS revenue
+    FROM customer_data
+    WHERE status = 'shipped'
+    GROUP BY week_of_year, item_id, customer_type
 )
-INSERT INTO mart.f_customer_retention
-SELECT DISTINCT
-    ncc.new_customers_count,
-    rcc.returning_customers_count,
-    rc.refunded_customer_count,
+INSERT INTO mart.f_customer_retention (new_customers_count, returning_customers_count, refunded_customer_count, period_name, period_id, item_id, new_customers_revenue, returning_customers_revenue, customers_refunded)
+SELECT 
+    COUNT(DISTINCT CASE WHEN customer_type = 'new_customer' THEN customer_id END) AS new_customers_count,
+    COUNT(DISTINCT CASE WHEN customer_type = 'returning_customer' THEN customer_id END) AS returning_customers_count,
+    COUNT(DISTINCT CASE WHEN customer_type = 'refunded_customer' THEN customer_id END) AS refunded_customer_count,
     'weekly' AS period_name,
-    dcl.week_of_year AS period_id,
-    di.item_id,
-    ncr.new_customers_revenue,
-    rcr.returning_customers_revenue,
-    crf.customers_refunded
-FROM mart.d_item di
-LEFT JOIN temp1 dcl ON 1 = 1
-LEFT JOIN new_customer_count2 ncc ON dcl.week_of_year = ncc.week_of_year AND di.item_id = ncc.item_id
-LEFT JOIN returning_customer_count2 rcc ON dcl.week_of_year = rcc.week_of_year AND di.item_id = rcc.item_id
-LEFT JOIN refunded_customer rc ON dcl.week_of_year = rc.week_of_year AND di.item_id = rc.item_id
-LEFT JOIN new_customers_revenue ncr ON dcl.week_of_year = ncr.week_of_year AND di.item_id = ncr.item_id
+    week_of_year,
+    item_id,
+    COALESCE(SUM(CASE WHEN customer_type = 'new_customer' THEN revenue END), 0) AS new_customers_revenue,
+    COALESCE(SUM(CASE WHEN customer_type = 'returning_customer' THEN revenue END), 0) AS returning_customers_revenue,
+    COALESCE(SUM(CASE WHEN customer_type = 'refunded_customer' THEN revenue END), 0) AS customers_refunded
+FROM customer_data
+LEFT JOIN revenue_data USING (week_of_year, item_id, customer_type)
+GROUP BY week_of_year, item_id;
